a93777a8822132440db557c0032b2a3773f27155
9 <nueve.sr@gmail.com>
2016-08-14 00:38:38 +0700

    Refactoring

--- KMCardSwipe/ViewController.swift
+++ KMCardSwipe/ViewController.swift
@@ -56,46 +56,6 @@ class ViewController: UIViewController {
 
 extension ViewController: UIScrollViewDelegate {
     
-    func next(view view: UIView) {
-        let height = CABasicAnimation(keyPath: "bounds.size.height")
-        height.fromValue = view.layer.bounds.height
-        height.toValue   = defaultHeightMax
-        
-        let width = CABasicAnimation(keyPath: "bounds.size.width")
-        width.fromValue = view.layer.bounds.width
-        width.toValue   = defaultWidthMax
-        
-        let groupAnimation =  CAAnimationGroup()
-        groupAnimation.animations = [height, width]
-        groupAnimation.removedOnCompletion = true
-        groupAnimation.duration = 0
-        groupAnimation.repeatCount = 1
-        
-        view.layer.addAnimation(groupAnimation, forKey: nil)
-        view.layer.bounds.size.height = defaultHeightMax
-        view.layer.bounds.size.width = defaultWidthMax
-    }
-    
-    func previous(view view: UIView) {
-        let height = CABasicAnimation(keyPath: "bounds.size.height")
-        height.fromValue = view.layer.bounds.height
-        height.toValue   = defaultHeightMin
-        
-        let width = CABasicAnimation(keyPath: "bounds.size.width")
-        width.fromValue = view.layer.bounds.width
-        width.toValue   = defaultWidthMin
-        
-        let groupAnimation =  CAAnimationGroup()
-        groupAnimation.animations = [height, width]
-        groupAnimation.removedOnCompletion = true
-        groupAnimation.duration = 0
-        groupAnimation.repeatCount = 1
-        
-        view.layer.addAnimation(groupAnimation, forKey: nil)
-        view.layer.bounds.size.height = defaultHeightMin
-        view.layer.bounds.size.width = defaultWidthMin
-    }
-    
     func scrollViewDidScroll(scrollView: UIScrollView) {
         let cardWidth = scrollView.frame.size.width;
         let fractionalCard = scrollView.contentOffset.x / cardWidth;
@@ -109,12 +69,43 @@ extension ViewController: UIScrollViewDelegate {
             self.currentIndex = cardIndex
             for (index, view) in self.card.enumerate() {
                 if index == cardIndex {
-                    self.next(view: view.subviews[0])
+                    self.animateCurrent(view: view.subviews[0])
                 } else {
-                    self.previous(view: view.subviews[0])
+                    self.animateOther(view: view.subviews[0])
                 }
             }
         }
     }
+    
+    // Animation
+    
+    func animateCurrent(view view: UIView) {
+        self.animateView(view: view, isCurrentView: true)
+    }
+    
+    func animateOther(view view: UIView) {
+        self.animateView(view: view, isCurrentView: false)
+    }
+    
+    func animateView(view view: UIView, isCurrentView: Bool) {
+        let height = CABasicAnimation(keyPath: "bounds.size.height")
+        height.fromValue = view.layer.bounds.height
+        height.toValue   = isCurrentView ? defaultHeightMax :defaultHeightMin
+        
+        let width = CABasicAnimation(keyPath: "bounds.size.width")
+        width.fromValue = view.layer.bounds.width
+        width.toValue   = isCurrentView ? defaultWidthMax : defaultWidthMin
+        
+        let groupAnimation =  CAAnimationGroup()
+        groupAnimation.animations = [height, width]
+        groupAnimation.removedOnCompletion = true
+        groupAnimation.duration = 0
+        groupAnimation.repeatCount = 1
+        
+        view.layer.addAnimation(groupAnimation, forKey: nil)
+        view.layer.bounds.size.height = isCurrentView ? defaultHeightMax : defaultHeightMin
+        view.layer.bounds.size.width = isCurrentView ? defaultWidthMax : defaultWidthMin
+    }
+
 }
 
